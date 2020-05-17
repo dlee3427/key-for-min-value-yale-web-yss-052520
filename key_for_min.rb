a1d251cea4661b5b54c68d8d215fae0487a45c37
David Lee <david.lee.sl2634@yale.edu>
2020-05-17 15:24:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  empty_hash = nil
+  lowest_value = name_hash[:name]
+  name_hash.find do |name, number|
+    if number < lowest_value 
+      lowest_value = number 
+      empty_hash = name
+    end
+  end
+  empty_hash
 end
