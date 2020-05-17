c16e7d0f6a22c84a56ab63014af4153f2b87986d
David Lee <david.lee.sl2634@yale.edu>
2020-05-17 15:43:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
 def key_for_min_value(name_hash)
   empty_hash = nil
   lowest_value = 0
-  name_hash.find do |name, number|
-    if number < lowest_value
+  name_hash.each do |name, number|
+    if number < lowest_value || lowest_value == 0
       lowest_value = number 
       empty_hash = name
     end
